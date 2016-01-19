6ede0731b1d37dc5733215a3b70cef2aa2018e03
jgaspard-fa <jgaspard@financeactive.com>
2016-01-19 19:59:12 +0100

    add test

---
+++ jee-module2/src/test/java/com/TestExample2.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestExample2 {
+
+    @Test
+    public void test(){
+
+        Assert.assertTrue(true);
+    }
+}
+
+
---
+++ jee-module2/src/test/java/com/TestExample3.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestExample3 {
+
+    /**
+     * release 0.35
+     */
+    @Test
+    public void test(){
+
+        Assert.assertTrue(true);
+    }
+}
+
+
