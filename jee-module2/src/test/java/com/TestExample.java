7e4af19480f84ffec1500c054a5c7db75d8acb61
jgaspard-fa <jgaspard@financeactive.com>
2016-01-19 14:54:21 +0100

    test

--- jee-module2/src/test/java/com/TestExample.java
+++ jee-module2/src/test/java/com/TestExample.java
@@ -1,12 +1,14 @@
 package com;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class TestExample {
 
+    @Test
     public void test(){
 
-        Assert.assertTrue(true);
+        Assert.assertTrue(false);
     }
 }
 
